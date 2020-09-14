50a1a9a0382835214d1425a60d110dbb28571ddc
Uday Kumar <udaykumaruking@gmail.com>
2020-09-14 20:33:40 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    contact()
+  );
+}
+
+
+class contact extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        //backgroundColor: Colors.blueGrey,
+        appBar: AppBar(
+          // elevation: 0,
+          title: Text("Contact Us", style: TextStyle(color: Colors.orange),),
+          backgroundColor: Colors.white,
+        ),
+        body: Column(
+          children: [
+            Image.asset('images/contact.jpg')
+          ],
+        ),
+      ),
+    );
+  }
+}
