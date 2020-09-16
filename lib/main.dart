540740896166095b1b95e1b683d2ae004e260b5b
Uday Kumar <udaykumaruking@gmail.com>
2020-09-16 17:32:27 +0530

    Added more widgets

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,8 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    contact()
-  );
+  runApp(contact());
 }
-
 
 class contact extends StatelessWidget {
   @override
@@ -14,19 +11,82 @@ class contact extends StatelessWidget {
       home: Scaffold(
         //backgroundColor: Colors.blueGrey,
         appBar: AppBar(
-          // elevation: 0,
-          title: Text("Contact Us", style: TextStyle(color: Colors.orange),),
+          elevation: 0,
+          title: Text(
+            "Contact Us",
+            style: TextStyle(color: Colors.orange),
+          ),
           backgroundColor: Colors.white,
         ),
         body: Column(
           children: [
-            Image.asset('images/contact.jpg')
+            SizedBox(
+              height: 50,
+            ),
+            Center(
+                child: Image.asset(
+              'images/contact.jpg',
+              height: 250,
+            )),
+            SizedBox(
+              height: 10,
+            ),
+            Text(
+              'If you need help \n feel free to contact us',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                color: Colors.grey[800],
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Container(
+                    height: 100,
+                    width: 150,
+                    color: Colors.orange,
+                    child: Text('container 1 '),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Container(
+                    height: 100,
+                    width: 150,
+                    color: Colors.orange,
+                    child: Text('container 2 '),
+                  ),
+                ),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Container(
+                    height: 100,
+                    width: 150,
+                    color: Colors.orange,
+                    child: Text('container 1 '),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Container(
+                    height: 100,
+                    width: 150,
+                    color: Colors.orange,
+                    child: Text('container 2 '),
+                  ),
+                ),
+              ],
+            )
           ],
         ),
       ),
     );
   }
 }
-
-
-//fcgvb
